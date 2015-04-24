0b2fc93d9c98e287091d1a3a1b148a26521dfe9a
Connor Abdelnoor & Tom Mertz <student@epicodus.com>
2015-04-23 18:00:24 -0700

    Calculate clues for each tile

--- controllers/TilesController.js
+++ controllers/TilesController.js
@@ -1,11 +1,18 @@
 mineSweeper.controller('TilesCtrl', function TilesCtrl($scope, TilesFactory) {
     $scope.tiles = TilesFactory.tiles;
 
+    $scope.tileContainer = {
+        "width": "80px",
+        "height": "80px",
+        "margin": "0 auto"
+    };
+
     $scope.rowLength = TilesFactory.rowLength;
 
     $scope.showTile = function(tile) {
-        tile.show = true;
-        console.log(tile);
+
+      tile.show = true;
+      console.log(tile);
     };
 
 });
--- services/TilesFactory.js
+++ services/TilesFactory.js
@@ -35,6 +35,7 @@ mineSweeper.factory('TilesFactory', function TilesFactory() {
 
         if(randomNumbers.indexOf(tile.id) !== -1) {
             tile.bomb = true;
+            tile.clue = "B";
         }
 
   // push each tile into the factory tile array
@@ -44,8 +45,67 @@ mineSweeper.factory('TilesFactory', function TilesFactory() {
 
   // set the clue number based on surrounding bombs
 
-  factory.tiles.forEach(function(tile) {
-    console.log("Row: " + tile.row + " Col: " + tile.col);
+  factory.tiles.forEach(function(tile, index, arr) {
+
+    //if we have a row above us
+    if(tile.row - 1 !== 0 ) {
+      //right above will exist and we check for a bomb
+      if(arr[tile.id - factory.rowLength].bomb) {
+        tile.clue++;
+      }
+
+      //check for a column to the left
+      if(tile.col - 1 !== 0) {
+        //if it exists check for a bomb
+        if(arr[tile.id - factory.rowLength - 1].bomb) {
+          tile.clue++;
+        }
+      }
+
+      //check for a column to the right
+      if(tile.col + 1 !== 5) {
+        //if it exists check for a bomb
+        if(arr[tile.id - factory.rowLength + 1].bomb) {
+          tile.clue++;
+        }
+      }
+    }
+    //if we have a row below us
+    if(tile.row + 1 !== 5) {
+      //we know the one below us exists so check for bomb
+      if(arr[tile.id + factory.rowLength].bomb) {
+        tile.clue++;
+      }
+
+      //check for column below to the left
+      if(tile.col - 1 !== 0) {
+        if(arr[tile.id + factory.rowLength - 1].bomb) {
+          tile.clue++;
+        }
+      }
+      //check for col below right
+      if(tile.col + 1 !== 5) {
+        if(arr[tile.id + factory.rowLength + 1].bomb) {
+          tile.clue++;
+        }
+      }
+    }
+    //if we have a col to the left of us
+    if(tile.col - 1 !== 0) {
+      //we have one to the left
+      if(arr[tile.id - 1].bomb) {
+        tile.clue++;
+      }
+    }
+    //if col is to the right
+    if(tile.col + 1 !== 5) {
+      //we have col to the right
+      if(arr[tile.id + 1].bomb) {
+        tile.clue++;
+      }
+    }
+
+    console.log("Clue: " + tile.clue);
 
   });
 
